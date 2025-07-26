d11eefcbe0a0fb8585cadb54fa86749ff37fe4c8
muhamedshijas <shijushijas157@gmail.com>
2025-07-26 08:16:06 +0530

    signup and login ui completed

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,36 @@
+export default function LoginPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-sm">
+        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        <form className="space-y-4">
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition"
+          >
+            Login
+          </button>
+          <div className="flex flex-row items-center gap-2">
+            <p>Don't have an account?</p>
+            <a
+              href="/signup"
+              className="text-blue-700 font-bold hover:underline"
+            >
+              Signup
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
